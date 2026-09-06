94f0e5c665d3dd2c763260cbb7d7cbc9d76d0d39
Farhaz Hussain <farhazhussain06@gmail.com>
2026-09-06 14:49:23 +0530

    Created register route

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
+from models import db, User
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///procurement.db'
+db.init_app(app)
 
-@app.route('/')
-def home():
-    return jsonify({"message": "Backend is running!"})
+with app.app_context():
+    db.create_all()
+
+@app.route('/register', methods=['POST'])
+def register():
+    data = request.json
+    new_user = User(
+        name=data['name'],
+        phone=data['phone'],
+        role=data.get('role', 'farmer')
+    )
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({"message": "Registered successfully", "user_id": new_user.id})
 
 if __name__ == '__main__':
     app.run(debug=True)
-    
